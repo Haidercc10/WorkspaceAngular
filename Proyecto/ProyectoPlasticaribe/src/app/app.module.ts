0f7ceb808c7aac228bef5238ab3acfddf0abd64e
Haidercc10 <cantillohaider10@gmail.com>
2022-04-21 17:57:14 -0500

    Cambio 2

--- Proyecto/ProyectoPlasticaribe/src/app/app.module.ts
+++ Proyecto/ProyectoPlasticaribe/src/app/app.module.ts
@@ -3,17 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { LoginComponentComponent } from './Vista/login-component/login-component.component';
 import { RegistroComponentComponent } from './Vista/registro-component/registro-component.component';
 import { PrincipalComponent } from './Vista/principal/principal.component';
 import { ReactiveFormsModule } from '@angular/forms';
-=======
-import { LoginComponentComponent } from './Vistas/login-component/login-component.component';
-import { RegistroComponentComponent } from './Vistas/registro-component/registro-component.component';
-import { PrincipalComponent } from './Vistas/principal/principal.component';
 
->>>>>>> 239d2cf5c50c7a2bea9f29dd6b8d00abd7f5e1f7
+
+
 
 
 export const routes: Routes = [
