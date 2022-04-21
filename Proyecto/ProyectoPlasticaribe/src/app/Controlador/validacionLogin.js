3105bda35c236ac1c782e16715c4dee5dea1b8f0
Diego Fernandez <diegofe21605@gmail.com>
2022-04-21 17:40:26 -0500

    Actualizacion

--- Proyecto/ProyectoPlasticaribe/src/app/Controlador/validacionLogin.js
+++ Proyecto/ProyectoPlasticaribe/src/app/Controlador/validacionLogin.js
@@ -1,12 +1,16 @@
-function validar (){
-
+function validar (identificacion){
     const { rest } = require('../Modelo/conexion.js'); //Llamado del archivo que contiene la conexión a la base de datos.
-
     setTimeout(async () => {
-        const resultado = await rest.executeQuery ('SELECT * FROM areas');
+        const resultado = await rest.executeQuery ('SELECT usu_id FROM usuarios');
         console.log(resultado.data);
     },50);
 
+    if (identificacion == resultado.data("usu_id")) {
+        
+    }    
+
 }
 
-validar();
+let identificacion = document.getElementById("identificacion");
+
+
