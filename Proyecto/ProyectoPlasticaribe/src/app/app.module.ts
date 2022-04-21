4647708425a39c76b01fbf3ae6be1abd18028dfc
Liseth Moreno <101366134+LisethMoreno1@users.noreply.github.com>
2022-04-21 10:58:28 -0500

    inicio

--- Proyecto/ProyectoPlasticaribe/src/app/app.module.ts
+++ Proyecto/ProyectoPlasticaribe/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
@@ -9,16 +8,24 @@ import { RegistroComponentComponent } from './registro-component/registro-compon
 import { PrincipalComponent } from './principal/principal.component';
 
 
+
 export const routes: Routes = [
+
   {path: '', component: LoginComponentComponent},
   {path: 'registro', component: RegistroComponentComponent},
-  {path: 'principal', component: PrincipalComponent}
+  {path: 'principal', component: PrincipalComponent},
+
 
 ]
 
 @NgModule({
   declarations: [
-    AppComponent, RegistroComponentComponent, LoginComponentComponent, PrincipalComponent],
+    AppComponent,
+    RegistroComponentComponent,
+    LoginComponentComponent,
+    PrincipalComponent],
+
+
   imports: [
     BrowserModule,
     AppRoutingModule,
