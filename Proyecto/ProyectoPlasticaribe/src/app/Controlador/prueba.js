108c6eff1acfcf2d0760ae0221b400af848799fc
Diego Fernandez <diegofe21605@gmail.com>
2022-04-21 16:28:31 -0500

    Consultas

--- Proyecto/ProyectoPlasticaribe/src/app/Controlador/prueba.js
+++
@@ -1,8 +0,0 @@
-
-const { rest } = require('../Modelo/conexion.js');
-
-setTimeout(async () => {
-    const resultado = await rest.executeQuery ('SELECT * FROM roles');
-    console.log(resultado.data);
-},15);
-
--- Proyecto/ProyectoPlasticaribe/src/app/Controlador/validacionLogin.js
+++ Proyecto/ProyectoPlasticaribe/src/app/Controlador/validacionLogin.js
@@ -1,11 +1,12 @@
-const { rest } = require('../Modelo/conexion.js'); //Llamado del archivo que contiene la conexión a la base de datos.
+function validar (){
 
+    const { rest } = require('../Modelo/conexion.js'); //Llamado del archivo que contiene la conexión a la base de datos.
 
-function validar (identificacion, contrasena, empresa){
     setTimeout(async () => {
-        const resultado = await rest.executeQuery ('SELECT * FROM roles');
+        const resultado = await rest.executeQuery ('SELECT * FROM areas');
         console.log(resultado.data);
-    },1);
+    },50);
+
 }
 
 validar();
