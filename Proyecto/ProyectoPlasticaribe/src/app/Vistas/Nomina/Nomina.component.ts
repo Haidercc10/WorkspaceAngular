974b1c1dd04d53739e1cfd7721efaae97977e779
Haidercc10 <cantillohaider10@gmail.com>
2023-06-17 12:53:09 -0500

    Avances Nómina

--- Proyecto/ProyectoPlasticaribe/src/app/Vistas/Nomina/Nomina.component.ts
+++ Proyecto/ProyectoPlasticaribe/src/app/Vistas/Nomina/Nomina.component.ts
@@ -13,10 +13,11 @@ import { AppComponent } from 'src/app/app.component';
 export class NominaComponent implements OnInit {
 
   FormEdicionMateriaPrima !: FormGroup;
-  @ViewChild('dtNomSellado') dt: Table | undefined;
+  @ViewChild('dtNomSellado') dtNomSellado: Table | undefined;
   @ViewChild('dtNomCorte') dtNomCorte: Table | undefined;
   @ViewChild('dtNomProduccion') dtNomProduccion: Table | undefined;
   @ViewChild('dtNomAdministrativo') dtNomAdministrativo: Table | undefined;
+  @ViewChild('dt') dt: Table | undefined;
   storage_Id : number; //Variable que se usará para almacenar el id que se encuentra en el almacenamiento local del navegador
   storage_Nombre : any; //Variable que se usará para almacenar el nombre que se encuentra en el almacenamiento local del navegador
   storage_Rol : any; //Variable que se usará para almacenar el rol que se encuentra en el almacenamiento local del navegador
@@ -26,17 +27,11 @@ export class NominaComponent implements OnInit {
   modoSeleccionado : boolean; //Variable que servirá para cambiar estilos en el modo oscuro/claro
   rangoFechas : any [] = []; /** Array que almacenará el rango de fechas */
   arraySellado : any = []; /** Array que cargará la información de la nomina de los operarios de sellado */
-  arrayCorte : any = []; /** Array que cargará la información de la nomina de los operarios de corte */
-  arrayProduccion : any = []; /** Array que cargará la información de la nomina de los operarios */
-  arrayAdministrativo : any = []; /** Array que cargará la información de la nomina de los administrativos */
   totalNominaSellado : number = 0; /** Variable que contendrá el valor total de la nomina de sellado */
-  totalNominaCorte : number = 0; /** Variable que contendrá el valor total de la nomina de corte */
-  totalNominaProduccion : number = 0; /** Variable que contendrá el valor total de la nomina de produccion */
-  totalNominaAdmin : number = 0; /** Variable que contendrá el valor total de la nomina de admininstrativos */
   modalSellado : boolean = false; /** Variable que contendrá el valor total de la nomina de sellado */
-  modalProduccion : boolean = false; /** Variable que contendrá el valor total de la nomina de produccion */
-  modalCorte : boolean = false; /** Variable que contendrá el valor total de la nomina de corte */
   operario : any = ''; /** Variable que cargará el ID y nombre del operario en cualquiera de los modales. */
+  detallesNomina : any [] = []; //Variable que almacenará la información detallada de los rollos pesados o ingresados de un producto y persona en especifico
+
 
   constructor(private AppComponent : AppComponent,
                 private servicioBagPro : BagproService) {
@@ -70,21 +65,15 @@ export class NominaComponent implements OnInit {
   exportarExcel(numero : number) {
   }
 
-  /** Función para consultar las nominas de forma categorizada por grupos */
+  /** Función para consultar las nomina de sellado */
   consultarNominas(){
     this.totalNominaSellado = 0;
-    this.totalNominaCorte = 0;
-    this.totalNominaProduccion = 0;
-    this.totalNominaAdmin = 0;
     this.arraySellado = [];
-    this.arrayCorte = [];
-    this.arrayProduccion = [];
-    this.arrayAdministrativo = [];
     let cedulas : any = [];
     let fechaInicial : any = this.rangoFechas.length > 0 ? moment(this.rangoFechas[0]).format('YYYY-MM-DD') : this.today;
     let fechaFinal : any = this.rangoFechas.length > 0 ? moment(this.rangoFechas[1]).format('YYYY-MM-DD') : fechaInicial;
 
-    this.servicioBagPro.getNominaSelladoAcumuladaItem('2023-06-01', '2023-06-10').subscribe(data => {
+    this.servicioBagPro.GetNominaSelladoAcumuladaItem('2023-06-01', '2023-06-10').subscribe(data => {
       for(let index = 0; index < data.length; index++) {
         let info : any = JSON.parse(`{${data[index].replaceAll("'", '"')}}`);
 
@@ -103,11 +92,12 @@ export class NominaComponent implements OnInit {
     });
     setTimeout(() => { this.cargarTabla2(); }, 1500);
   }
-  /** Función para cargar las tablas de Nóminas */
+
+  /** Función para cargar la tabla de Nómina detallada */
   cargarTabla2(){
     let array : any = [];
 
-    this.servicioBagPro.getNominaSelladoAcumuladaItem('2023-06-01', '2023-06-10').subscribe(data => {
+    this.servicioBagPro.GetNominaSelladoAcumuladaItem('2023-06-01', '2023-06-10').subscribe(data => {
       for(let index = 0; index < data.length; index++) {
         let info : any = JSON.parse(`{${data[index].replaceAll("'", '"')}}`);
         info.Cedula = info.Cedula,
@@ -116,26 +106,50 @@ export class NominaComponent implements OnInit {
         info.PagoTotal = parseFloat(info.PagoTotal),
         info.Cantidad = parseFloat(info.Cantidad),
         info.CantidadTotal = parseFloat(info.CantidadTotal),
+        info.PrecioDia = parseFloat(info.PrecioDia),
+        info.PrecioNoche = parseFloat(info.PrecioNoche),
         info.detalle = [];
 
         array = this.arraySellado.findIndex(item => item.Cedula == info.Cedula);
-
+        console.log(array)
         if(array >= 0) this.arraySellado[array].detalle.push(info);
       }
     });
   }
 
-  /** Funcion para filtrar busquedas y mostrar datos segun el filtro consultado. */
-  aplicarfiltro($event, campo : any, valorCampo : string){
-    this.dt!.filter(($event.target as HTMLInputElement).value, campo, valorCampo);
+  cargarValorADevengar(){
+
   }
 
-  /** Cargará el modal de sellado al momento de seleccionar un usuario en la columna ver detalle de la tabla */
-  cargarModalSellado = () => this.modalSellado = true;
+  /** Funcion para filtrar busquedas y mostrar datos segun el filtro consultado. */
+  aplicarfiltro($event, campo : any, valorCampo : string){
+    this.dtNomSellado!.filter(($event.target as HTMLInputElement).value, campo, valorCampo);
+  }
 
-   /** Cargará el modal de corte al momento de seleccionar un usuario en la columna ver detalle de la tabla  */
-  cargarModalCorte = () => this.modalCorte = true;
+  /** Cargará el modal de sellado al momento de seleccionar un operario en la columna ver detalle de la tabla */
+  cargarModalSellado(item : any, persona : string){
+    //this.load = true;
+    this.modalSellado = true;
+    this.detallesNomina = [];
+    let fechaInicial : any = this.rangoFechas.length > 0 ? moment(this.rangoFechas[0]).format('YYYY-MM-DD') : this.today;
+    let fechaFinal : any = this.rangoFechas.length > 0 ? moment(this.rangoFechas[1]).format('YYYY-MM-DD') : fechaInicial;
 
-   /** Cargará el modal de produccion al momento de seleccionar un usuario en la columna ver detalle de la tabla */
-  cargarModalProduccion = () => this.modalProduccion = true;
+    this.servicioBagPro.GetNominaSelladoDetalladaItemPersona('2023-06-01', '2023-06-10', item, persona).subscribe(data => {
+      for (let i = 0; i < data.length; i++) {
+        let info : any = JSON.parse(`{${data[i].replaceAll("'", '"')}}`);
+        info.Fecha = info.Fecha.replace('12:00:00 a. m. ', '');
+        info.Cantidad = parseFloat(info.Cantidad.toString().replace(',', '.')),
+        info.Peso = parseFloat(info.Peso.toString().replace(',', '.')),
+        info.Precio = parseFloat(info.Precio.toString().replace(',', '.'));
+        info.Valor_Total = parseFloat(info.Valor_Total.toString().replace(',', '.'));
+        info.Pesado_Entre = parseFloat(info.Pesado_Entre.toString().replace(',', '.'));
+        info.Cantidad_Total = parseFloat(info.Cantidad_Total.toString().replace(',', '.'));
+        this.detallesNomina.push(info);
+      }
+    });
+    //setTimeout(() => this.load = false, 2000);
+  }
+
+  /** Filtrar la tabla detallada del modal de sellado */
+  aplicarfiltro2 = ($event, campo : any, valorCampo : string) => this.dt!.filter(($event.target as HTMLInputElement).value, campo, valorCampo);
 }
