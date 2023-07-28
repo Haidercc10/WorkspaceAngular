abbb645ca9859f12e939348c68509978ea29ad85
Diego Fernandez <diegofe21605@gmail.com>
2023-07-28 15:51:25 -0500

    Corrección Descarga de Archivos
    Los archivos se estaban descargando duplicados.
    Se corrigió y ahora solo se descarga un archivo.

--- Proyecto/ProyectoPlasticaribe/src/app/_helpers/jwt.interceptor_BagPro.ts
+++ Proyecto/ProyectoPlasticaribe/src/app/_helpers/jwt.interceptor_BagPro.ts
@@ -1,22 +1,18 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { authentication_BagPro } from '../_Services/authentication_BagPro.service';
-import { rutaBagPro } from 'src/polyfills';
 import { SESSION_STORAGE, WebStorageService } from 'ngx-webstorage-service';
+import { Observable } from 'rxjs';
+import { rutaBagPro } from 'src/polyfills';
 import { EncriptacionService } from '../Servicios/Encriptacion/Encriptacion.service';
 
 @Injectable()
 
 export class jwtInterceptor_BagPro implements HttpInterceptor {
 
-  constructor(private authenticationService: authentication_BagPro,
-                @Inject(SESSION_STORAGE) private storage: WebStorageService,
-                  private encriptacion : EncriptacionService,) { }
+  constructor(@Inject(SESSION_STORAGE) private storage: WebStorageService,
+                private encriptacion : EncriptacionService,) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const user = this.authenticationService.userValue;
-    const isLoggedIn = user?.token;
     const token = this.encriptacion.decrypt(this.storage.get('Token_BagPro') == undefined ? '' : this.storage.get('Token_BagPro'));
     const isApiUrl = request.url.startsWith(rutaBagPro);
     if (token && isApiUrl) request = request.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
--- Proyecto/ProyectoPlasticaribe/src/app/_helpers/jwt.interceptor_ContaZeus.ts
+++ Proyecto/ProyectoPlasticaribe/src/app/_helpers/jwt.interceptor_ContaZeus.ts
@@ -1,22 +1,18 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { SESSION_STORAGE, WebStorageService } from 'ngx-webstorage-service';
 import { Observable } from 'rxjs';
 import { rutaZeusContabilidad } from 'src/polyfills';
-import { SESSION_STORAGE, WebStorageService } from 'ngx-webstorage-service';
-import { authentication_ContaZeus } from '../_Services/authentication_ContaZeus.service';
 import { EncriptacionService } from '../Servicios/Encriptacion/Encriptacion.service';
 
 @Injectable()
 
 export class JwtInterceptor_ContaZeus implements HttpInterceptor {
 
-  constructor(private authenticationService: authentication_ContaZeus,
-                @Inject(SESSION_STORAGE) private storage: WebStorageService,
-                  private encriptacion : EncriptacionService,) { }
+  constructor(@Inject(SESSION_STORAGE) private storage: WebStorageService,
+                private encriptacion : EncriptacionService,) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const user = this.authenticationService.userValue;
-    const isLoggedIn = user?.token;
     const token = this.encriptacion.decrypt(this.storage.get('Token_Conta_Zeus') == undefined ? '' : this.storage.get('Token_Conta_Zeus'));
     const isApiUrl = request.url.startsWith(rutaZeusContabilidad);
     if (token && isApiUrl) request = request.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
