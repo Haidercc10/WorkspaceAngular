0df8ca63662d8ec4cea6330204f708a0882cedb0
Haidercc10 <cantillohaider10@gmail.com>
2023-08-14 10:21:17 -0500

    Cambios

--- Proyecto/ProyectoPlasticaribe/src/app/Vistas/prueba-imagen-cat-insumo/prueba-imagen-cat-insumo.component.ts
+++ Proyecto/ProyectoPlasticaribe/src/app/Vistas/prueba-imagen-cat-insumo/prueba-imagen-cat-insumo.component.ts
@@ -46,6 +46,7 @@ export class PruebaImagenCatInsumoComponent implements OnInit {
                     private srvRoles : RolesService,){
 
     this.formVistas = this.frmBuilder.group({
+      vId: [null],
       vNombre: [null, Validators.required],
       vIcono: [null, Validators.required],
       vDock: [null],
@@ -59,7 +60,6 @@ export class PruebaImagenCatInsumoComponent implements OnInit {
     this.cargarVistasDistintas();
     this.cargarRoles();
     this.iconos2 = iconos;
-    console.log(this.iconos2)
   }
 
   cargarVistasDistintas() {
@@ -79,6 +79,7 @@ export class PruebaImagenCatInsumoComponent implements OnInit {
     if(this.formVistas.valid) {
       if(!this.formVistas.value.vRuta.toString().includes(' ')) {
         let modelo : modelVistasPermisos = {
+          Vp_Id : this.formVistas.value.vId,
           Vp_Nombre: this.formVistas.value.vNombre,
           Vp_Icono_Dock: this.rutaImagenes + `${this.formVistas.value.vDock}`,
           Vp_Icono_Menu: this.formVistas.value.vIcono,
@@ -86,9 +87,18 @@ export class PruebaImagenCatInsumoComponent implements OnInit {
           Vp_Categoria: `|${this.formVistas.value.vCategoria.toString().replaceAll(',', '|')}|`,
           Vp_Id_Roles: `|${this.formVistas.value.vRoles.toString().replaceAll(',', '|')}|`,
         }
-        this.srvVistaPermisos.Post(modelo).subscribe(data => { this.msjs.mensajeConfirmacion(`Excelente!`, `Se ha guardado la vista ${modelo.Vp_Nombre} exitosamente!`) },
-        error => { this.msjs.mensajeConfirmacion(`Error`, `No fue posible crear la vista ${modelo.Vp_Nombre}, por favor, verifique!`); });
-
+        if(this.palabra == 'Crear') {
+          console.log(1)
+          console.log(modelo)
+          //this.srvVistaPermisos.Post(modelo).subscribe(data => { this.msjs.mensajeConfirmacion(`Excelente!`, `Se ha guardado la vista ${modelo.Vp_Nombre} exitosamente!`) },
+          //error => { this.msjs.mensajeConfirmacion(`Error`, `No fue posible crear la vista ${modelo.Vp_Nombre}, por favor, verifique!`); });
+        }
+        if(this.palabra == 'Editar') {
+          console.log(2)
+          console.log(modelo)
+          //this.srvVistaPermisos.Put(modelo.Vp_Id, modelo).subscribe(data => { this.msjs.mensajeConfirmacion(`Excelente!`, `Se ha guardado la vista ${modelo.Vp_Nombre} exitosamente!`) },
+          //error => { this.msjs.mensajeConfirmacion(`Error`, `No fue posible editar la vista ${modelo.Vp_Nombre}, por favor, verifique!`); });
+        }
       } else this.msjs.mensajeAdvertencia(`Advertencia`, `Las rutas no pueden contener espacios, por favor, verifique!`);
     } else this.msjs.mensajeAdvertencia(`Advertencia`, `Debe completar los campos vacios!`);
   }
@@ -126,24 +136,36 @@ export class PruebaImagenCatInsumoComponent implements OnInit {
   cargarVistas_Id(vista : any){
     let imagen : any;
     let icono : any;
-    let roles : any;
-    this.categorias
+    let roles : any = [];
+    let rolesCargar : any = [];
+    let categorias : any = [];
     this.modal = true;
     this.palabra = `Editar`;
+    console.log(this.palabra)
 
     this.srvVistaPermisos.Get_By_Id(vista.Id).subscribe(data => {
+
+      roles = data.vp_Id_Roles.split('|'); roles.shift(); roles.pop();
+      categorias = data.vp_Categoria.split('|'); categorias.shift(); categorias.pop();
+      roles.forEach(element => { this.srvRoles.srvObtenerListaPorId(element).subscribe(data => { if(data.rolUsu_Nombre != undefined) rolesCargar.push(data.rolUsu_Id) }); });
       imagen = this.imagenes.filter(item => item.nombre == data.vp_Icono_Dock.replace('assets/Iconos_Menu/', ''));
       icono = this.iconos2.filter(item => item.icon == data.vp_Icono_Menu);
 
       this.formVistas.patchValue({
+        vId : data.vp_Id,
         vNombre : data.vp_Nombre,
         vIcono : icono[0].icon,
         vDock : imagen[0].nombre,
-        vCategoria : data.vp_Categoria,
+        vCategoria : categorias,
         vRuta : data.vp_Ruta,
-        vRoles : data.vp_Id_Roles,
+        vRoles : rolesCargar,
       });
     });
   }
 
+  editarVistas() {
+
+  }
+
 }
+
