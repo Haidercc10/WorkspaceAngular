de4109a9de6ec29da0d4bc3c789044e46747c071
Haidercc10 <cantillohaider10@gmail.com>
2023-09-21 10:58:50 -0500

    Cambios

--- Proyecto/ProyectoPlasticaribe/src/app/Vistas/Inventario_Areas/Inventario_Areas.component.ts
+++ Proyecto/ProyectoPlasticaribe/src/app/Vistas/Inventario_Areas/Inventario_Areas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import moment from 'moment';
+import { MessageService } from 'primeng/api';
 import { modelInventario_Areas } from 'src/app/Modelo/modelInventario_Areas';
 import { BagproService } from 'src/app/Servicios/BagPro/Bagpro.service';
 import { Inventario_AreasService } from 'src/app/Servicios/Inventario_Areas/Inventario_Areas.service';
@@ -27,11 +28,15 @@ export class Inventario_AreasComponent implements OnInit {
   modoSeleccionado : boolean; //Variable que servirá para cambiar estilos en el modo oscuro/claro
   materiasPrimas = []; //Variable que va almacenar la información de las materias primas de extrusion
   procesos = []; //Variable que va almacenar la información de todas las unidades de medida
-  formulario !: FormGroup;
-  ordenes_trabajos : any = [];
-  inventario : any = [];
-  area : string = "";
-  productos : any = []; 
+  formulario !: FormGroup; //Formulario de la vista
+  ordenes_trabajos : any = []; //Array que guardará las diferentes OT
+  inventario : any = []; //Array que guardará la información del inventario que se está cargando
+  area : string = ""; //Variable que guardará el area del usuario logueado segun su rol. 
+  productos : any = []; //Variable que guardará la información de los productos
+  nroFilas : number = 8; //Variable que guardará el numero de filas que ocupará el campo observación
+  registroSeleccionado : any;
+  contador : number = 0;
+  titulo = `Inventarios Areas`;
 
   constructor(private AppComponent : AppComponent,
                private frmBuilder : FormBuilder, 
@@ -39,7 +44,8 @@ export class Inventario_AreasComponent implements OnInit {
                   private svcBagPro : BagproService,
                     private svcProcesos : ProcesosService, 
                       private svcMsjs : MensajesAplicacionService, 
-                        private svcInventario : Inventario_AreasService) {
+                        private svcInventario : Inventario_AreasService, 
+                          private msg : MessageService) {
 
     this.modoSeleccionado = this.AppComponent.temaSeleccionado;
     this.formulario = this.frmBuilder.group({
@@ -59,6 +65,7 @@ export class Inventario_AreasComponent implements OnInit {
     this.filtrarArea();
     this.cargarProcesos();
     if([7, 3, 1].includes(this.ValidarRol)) this.cargarMateriasPrimas();
+    this.ValidarRol == 1 ? this.nroFilas = 8 : this.nroFilas = 12;
   }
 
   //Funcion que leerá la informacion que se almacenará en el storage del navegador
@@ -70,22 +77,26 @@ export class Inventario_AreasComponent implements OnInit {
 
   //Función que cargará el area del usuario logueado
   filtrarArea(){
-    this.ValidarRol == 3 || this.ValidarRol ==  7 ? this.area = "EXT" : 
-    this.ValidarRol == 8 ? this.area = "SELLA" :
-    this.ValidarRol == 63 ? this.area = "ROT" :
-    this.ValidarRol == 62 ? this.area = "IMP" : this.area = "N/A";
+    if(this.ValidarRol == 3 || this.ValidarRol ==  7) { this.area = "EXT"; this.titulo = `Inventario Extrusión`; } 
+    else if (this.ValidarRol == 8) { this.area = "SELLA"; this.titulo = `Inventario Sellado`; }
+    else if (this.ValidarRol == 63) { this.area = "ROT"; this.titulo = `Inventario Rotograbado`; }
+    else if (this.ValidarRol == 62) {this.area = "IMP"; this.titulo = `Inventario Impresión`; }  
+    else { this.area = null; this.titulo = `Inventarios Areas`; } 
   }
 
   //Función que cargará la información de las materias primas
   cargarMateriasPrimas = () => this.svcMatPrimas.srvObtenerLista().subscribe(datos => this.materiasPrimas = datos);
    
-  //Función que cargará la información de las materias primas
+  //Función que cargará la información de los procesos/areas
   cargarProcesos() {
     let proceso : any = [];
     this.svcProcesos.srvObtenerLista().subscribe(datos => { 
       this.procesos = datos; 
-      proceso = this.procesos.filter(x => x.proceso_Id == this.area);
-      this.formulario.patchValue({ proceso : proceso[0].proceso_Id });
+      this.procesos = this.procesos.filter(x => [`EXT`, `IMP`, `ROT`, `SELLA`].includes(x.proceso_Id))
+      if(this.ValidarRol != 1) {
+        proceso = this.procesos.filter(x => x.proceso_Id == this.area);
+        this.formulario.patchValue({ proceso : proceso[0].proceso_Id });
+      }
     }, error => { this.svcMsjs.mensajeError(`Error`, `Error al cargar las areas`) }); 
   }
 
@@ -97,9 +108,9 @@ export class Inventario_AreasComponent implements OnInit {
     else {
       if(ot != null) {
         this.load = true;
-        fecha = moment(fecha).format('YYYY-MM-DD');
+        //fecha = moment(fecha).format('YYYY-MM-DD');
         this.svcBagPro.srvObtenerListaClienteOT_Item(this.formulario.value.ot).subscribe(data => { 
-          if(data.length > 0) this.cargarCampos(data[0]); 
+          if(data.length > 0) this.cargarCampos(data[0]);  
           else {
             this.load = false;
             this.svcMsjs.mensajeAdvertencia(`Advertencia`, `La OT N° ${ot} no existe!`);
@@ -110,7 +121,7 @@ export class Inventario_AreasComponent implements OnInit {
     } 
   }
 
-  //Función que cargará la info de la OT en los campos
+  //Función que cargará la info de la OT en los campos del formulario
   cargarCampos(data : any) {
     this.formulario.patchValue({
       item : data.clienteItems,
@@ -121,18 +132,17 @@ export class Inventario_AreasComponent implements OnInit {
     this.load = false;
   }
 
-  //Función que enviará el registro de inventario
+  //Función que enviará el registro de inventario a la tabla
   enviarRegistro(){
     let cantidad : number = this.formulario.value.cantidad;
     let proceso : any = this.formulario.value.proceso;
     let observacion : any = this.formulario.value.observacion == null ? '' : this.formulario.value.observacion;
     let ot : any = this.formulario.value.ot == null || this.formulario.value.ot == '' ? 0 : this.formulario.value.ot;
-    
     if(this.formulario.valid) {
       if(cantidad > 0) {
-        if(proceso != null && proceso != 'N/A') {
+        if(![null, "N/A"].includes(proceso)) {
           let info : modelInventario_Areas = {
-            InvCodigo : 0,
+            InvCodigo : this.contador += 1,
             OT: ot,
             Prod_Id: this.formulario.value.item,
             Referencia : this.formulario.value.referencia,
@@ -148,7 +158,6 @@ export class Inventario_AreasComponent implements OnInit {
             Usua_Id: this.storage_Id,
             InvObservacion: observacion
           };
-          
           if(!this.ordenes_trabajos.includes(info.OT)) {
             this.ordenes_trabajos.push(info.OT);
             this.inventario.push(info);
@@ -161,6 +170,7 @@ export class Inventario_AreasComponent implements OnInit {
     
   }
 
+  //Función que calcula el precio total de las referencias de la tabla
   precioTotal(){
     let total : number = 0;
     for (const item of this.inventario) {
@@ -169,8 +179,25 @@ export class Inventario_AreasComponent implements OnInit {
     return total;  
   }
 
-  mostrarEleccion(data : any){}
+  //Función que mostrará el mensaje de elección de eliminación de un registro de la tabla
+  mostrarEleccion(data : any){
+    this.registroSeleccionado = data;
+    this.msg.add({ key: 'eleccion', severity:'warn', summary:'Elección', detail: `Está seguro que desea quitar la referencia ${data.Referencia} de la tabla?`, sticky: true});
+  }
 
+  //Función que quitará el mensaje de elección de eliminación de un registro de la tabla
+  onReject = () => this.msg.clear('eleccion');
+
+  //Función que eliminará un registro de la tabla
+  eliminarRegistro(){
+    this.onReject();
+    this.ordenes_trabajos.splice(this.ordenes_trabajos.findIndex(x => x == this.registroSeleccionado.OT), 1);
+    this.inventario.splice(this.inventario.findIndex(x => x.InvCodigo == this.registroSeleccionado.InvCodigo), 1);
+    this.svcMsjs.mensajeConfirmacion(`¡Confirmación!`, `Se eliminó la referencia ${this.registroSeleccionado.Referencia} de la tabla!`);
+    this.registroSeleccionado = null;
+  }
+
+  //Función que limpiará los campos del formulario
   limpiarCampos(){
     this.formulario.patchValue({
       ot : null,
@@ -182,15 +209,15 @@ export class Inventario_AreasComponent implements OnInit {
     });
   }  
 
+  //Función que creará la entrada de inventario en la base de datos
   crearEntrada() {
     this.load = true;
     let esError : boolean = false;
     this.inventario.forEach(x => { 
       x.InvFecha_Registro = this.today;
       x.InvHora_Registro = this.hora;
-      delete x.Subtotal;
-      delete x.Referencia;
-      console.log(x);
+      x.InvCodigo = 0;
+      delete x.Subtotal, x.Referencia;
       this.svcInventario.Post(x).subscribe(data => { esError = false }, error => { esError = true });
     });
     setTimeout(() => {
@@ -198,9 +225,9 @@ export class Inventario_AreasComponent implements OnInit {
       (esError) ? this.svcMsjs.mensajeError(`Error`, `No se pudo crear la entrada de inventario!`) : this.svcMsjs.mensajeConfirmacion(`¡Sí!`, `Se creó la entrada de inventario de forma exitosa!`);
       this.limpiarTodo();
     }, 2000);
-  
   }
 
+  //Función que limpiará todos los campos del formulario y la tabla
   limpiarTodo() {
     this.formulario.patchValue({
       ot : null,
@@ -212,13 +239,16 @@ export class Inventario_AreasComponent implements OnInit {
     });
     this.inventario = [];
     this.ordenes_trabajos = [];
+    this.contador = 0;
   }
 
+  //Función que consultará las referencias de productos/materias primas
   consultarReferencia(){
     let referencia : any = this.formulario.value.referencia;
     if(referencia != null && referencia.length > 2) this.svcBagPro.LikeReferencia(referencia).subscribe(data => this.productos = data);
   }
 
+  //Funcion que seleccionará una referencia y cargará su id y precio 
   seleccionarReferencia(){
     let ref : any = [];
     ref = this.productos.filter((item) => item.referencia == this.formulario.value.referencia);
