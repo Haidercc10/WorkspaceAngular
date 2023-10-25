3c25aa05a1aced546ab18223bcfc37e72f72d8fe
Haidercc10 <cantillohaider10@gmail.com>
2023-10-25 08:54:25 -0500

    Cambios

--- Proyecto/ProyectoPlasticaribe/src/environments/environment.prod.ts
+++ Proyecto/ProyectoPlasticaribe/src/environments/environment.prod.ts
@@ -1,12 +1,7 @@
 export const environment = {
   production: true,
-  //rutaPlasticaribeAPI : "http://192.168.0.85:9086/api",
-  //rutaBagPro : "http://192.168.0.85:9095/api",
-  //rutaZeus : "http://192.168.0.85:9055/api",
-  //rutaZeusContabilidad : "http://192.168.0.85:9065/api",
-
-  rutaPlasticaribeAPI : "http://192.168.0.137:9086/api",
-  rutaBagPro : "http://192.168.0.137:9095/api",
-  rutaZeus : "http://192.168.0.137:9055/api",
-  rutaZeusContabilidad : "http://192.168.0.137:9065/api"
+  rutaPlasticaribeAPI : "http://192.168.0.85:9086/api",
+  rutaBagPro : "http://192.168.0.85:9095/api",
+  rutaZeus : "http://192.168.0.85:9055/api",
+  rutaZeusContabilidad : "http://192.168.0.85:9065/api",
 };
