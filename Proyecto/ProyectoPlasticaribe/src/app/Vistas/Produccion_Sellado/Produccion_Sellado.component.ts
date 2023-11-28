4e15af311109a11dcf98488004524b4ab6cf4dbd
Haidercc10 <cantillohaider10@gmail.com>
2023-11-28 12:27:58 -0500

    Cambios

--- Proyecto/ProyectoPlasticaribe/src/app/Vistas/Produccion_Sellado/Produccion_Sellado.component.ts
+++ Proyecto/ProyectoPlasticaribe/src/app/Vistas/Produccion_Sellado/Produccion_Sellado.component.ts
@@ -5,6 +5,7 @@ import { Table } from 'primeng/table';
 import { modelProduccionProcesos } from 'src/app/Modelo/modelProduccionProcesos';
 import { modelProduccion_Procesos } from 'src/app/Modelo/modelProduccion_Procesos';
 import { BagproService } from 'src/app/Servicios/BagPro/Bagpro.service';
+import { CreacionPdfService, modelTagProduction } from 'src/app/Servicios/CreacionPDF/creacion-pdf.service';
 import { MensajesAplicacionService } from 'src/app/Servicios/MensajesAplicacion/MensajesAplicacion.service';
 import { Produccion_ProcesosService } from 'src/app/Servicios/Produccion_Procesos/Produccion_Procesos.service';
 import { TurnosService } from 'src/app/Servicios/Turnos/Turnos.service';
@@ -30,6 +31,8 @@ export class Produccion_SelladoComponent implements OnInit {
   hora : any = moment().format('hh:mm:ss');
   esSoloLectura : boolean = true;  
   ordenConsultada : any;
+  procesos : any = [{Id : 'SELLA', Nombre: 'SELLADO'}, {Id : 'WIKE', Nombre: 'WIKETIADO'}]
+  clase : any = ``
 
   constructor(private AppComponent : AppComponent, 
   private svcTurnos : TurnosService,
@@ -37,7 +40,8 @@ export class Produccion_SelladoComponent implements OnInit {
   private svcUsuarios : UsuarioService, 
   private svcBagPro : BagproService, 
   private svcMsjs : MensajesAplicacionService, 
-  private svcProdProcesos : Produccion_ProcesosService) { 
+  private svcProdProcesos : Produccion_ProcesosService, 
+  private svcCrearPDF : CreacionPdfService, ) { 
     this.modoSeleccionado = this.AppComponent.temaSeleccionado;
     this.inicializarForm();
   }
@@ -60,6 +64,7 @@ export class Produccion_SelladoComponent implements OnInit {
       pesoNeto : [null],
       maquina : [null],
       saldo : [false],
+      proceso : ['SELLA'],
     });
   }
 
@@ -76,7 +81,12 @@ export class Produccion_SelladoComponent implements OnInit {
         this.cargando = true;
         this.ordenConsultada = this.formSellado.value.ot;
         this.ordenesTrabajo = data; 
+        console.log(this.ordenesTrabajo)
         this.formSellado.patchValue({ cantUnd : data[0].selladoCorte_CantBolsasBulto }); 
+        if (data[0].cantidad_Sellado == 0) this.clase = `badge bg-rojo`;
+        else if (data[0].cantidad_Sellado > 0 && data[0].cantidad_Sellado < data[0].cantidad_Pedida) this.clase = `badge bg-amarillo`;
+        else if(data[0].cantidad_Sellado >= data[0].cantidad_Pedida) this.clase = `badge bg-verde`;
+        else this.clase = ``;
         this.cargarProduccionSellado(this.formSellado.value.ot)
       }
     }, error => { this.ordenesNoEncontradas(); });
@@ -90,10 +100,7 @@ export class Produccion_SelladoComponent implements OnInit {
         this.formSellado.patchValue({ cantUnd : qty == 0 ? data[0].cantidadUnd : qty });
         this.produccion.sort((a, b) => a.bulto - b.bulto);
         this.cargando = false;
-      } else {
-        this.svcMsjs.mensajeAdvertencia(`La OT ${ot} no tiene producción en el proceso de Sellado`);
-        this.cargando = false;
-      } 
+      } else this.cargando = false;
     }, error => { this.svcMsjs.mensajeError(`La OT ${ot} no fue encontrada en el proceso de Sellado`) });
   }
 
@@ -120,13 +127,9 @@ export class Produccion_SelladoComponent implements OnInit {
         if(this.formSellado.value.idOperario != null) {
           if( (this.formSellado.value.idOperario).length < 5) {
             if(this.formSellado.value.cantUnd > 0) {
-              if(this.formSellado.value.cantKg > 0) {
-                if(this.formSellado.value.pesoTeorico > 0) {
-                  if(this.formSellado.value.pesoNeto > 0) {
-                    this.crearEntrada(this.ordenesTrabajo[0]);
-                  } else this.svcMsjs.mensajeAdvertencia(`Advertencia`, `El peso real debe ser mayor a '0'!`);
-                }  else this.svcMsjs.mensajeAdvertencia(`Advertencia`, `El peso teórico debe ser mayor a '0'!`);
-              }  else this.svcMsjs.mensajeAdvertencia(`Advertencia`, `La cantidad en kilos debe ser mayor que '0'!`);
+              if(this.formSellado.value.cantKg > -2) {
+                this.crearEntrada(this.ordenesTrabajo[0]);
+              } else this.svcMsjs.mensajeAdvertencia(`Advertencia`, `La cantidad en kilos debe ser mayor que '0'!`);
             } else this.svcMsjs.mensajeAdvertencia(`Advertencia`, `La cantidad en unidades/paquetes debe ser mayor a '0'!`);
           } else this.svcMsjs.mensajeAdvertencia(`Advertencia`, `Un rollo no puede ser pesado por más de 4 operarios, verifique!`);
         } else this.svcMsjs.mensajeAdvertencia(`Advertencia`, `Debe seleccionar al menos un operario!`);  
@@ -151,13 +154,13 @@ export class Produccion_SelladoComponent implements OnInit {
       'Ancho_Cono': 0,
       'Tara_Cono': 0,
       'Peso_Bruto': parseFloat(this.formSellado.value.cantKg),
-      'Peso_Neto': parseFloat(this.formSellado.value.pesoNeto),
-      'Cantidad': this.formSellado.value.cantUnd,
-      'Peso_Teorico': (orden.selladoCorte_PesoMillar * this.formSellado.value.cantUnd) / 1000,
+      'Peso_Neto': parseFloat(this.formSellado.value.cantKg),
+      'Cantidad': parseFloat(this.formSellado.value.cantUnd),
+      'Peso_Teorico': 0,
       'Desviacion': 0,
       'Precio': orden.valorUnidad,
       'Presentacion': orden.presentacion == 'Unidad' ? 'Und' : orden.presentacion == 'Kilo' ? 'Kg' : orden.presentacion,
-      'Proceso_Id': 'SELLA' || 'WIKE',
+      'Proceso_Id': this.formSellado.value.proceso,
       'Turno_Id': this.formSellado.value.turno,
       'Envio_Zeus': false,
       'Datos_Etiqueta': `${orden.selladoCorte_Ancho} X ${orden.selladoCorte_Largo}`,
@@ -165,14 +168,32 @@ export class Produccion_SelladoComponent implements OnInit {
       'Hora': this.hora,
       'Creador_Id': this.AppComponent.storage_Id,
     }
-    if(entrada.Presentacion == 'Kg') entrada.Desviacion = 0;
-    else if(entrada.Presentacion == 'Und' || entrada.Presentacion == 'Paquete') entrada.Desviacion = ((entrada.Peso_Bruto - entrada.Peso_Teorico) * 100) / entrada.Peso_Teorico;
-    
+    if(entrada.Presentacion == 'Kg') {
+      entrada.Peso_Teorico = entrada.Peso_Bruto;
+      entrada.Cantidad = entrada.Peso_Bruto;
+      entrada.Desviacion = 0;
+    } else if(entrada.Presentacion == 'Und') {
+      entrada.Peso_Teorico = ((orden.selladoCorte_PesoMillar * this.formSellado.value.cantUnd) / 1000); 
+      entrada.Desviacion = ((entrada.Peso_Bruto - entrada.Peso_Teorico) * 100) / entrada.Peso_Teorico;
+    } else if(entrada.Presentacion == 'Paquete' && entrada.Cantidad == 1) {
+      entrada.Peso_Teorico = (orden.selladoCorte_PesoMillar * this.formSellado.value.cantUnd); 
+      entrada.Desviacion = ((entrada.Peso_Bruto - entrada.Peso_Teorico) * 100) / entrada.Peso_Teorico;
+    } else if(entrada.Presentacion == 'Paquete' && entrada.Cantidad > 1) {
+      entrada.Peso_Teorico = ((entrada.Cantidad * orden.selladoCorte_PesoMillar * orden.selladoCorte_CantBolsasPaquete) / 1000); 
+      entrada.Desviacion = 0;
+    }
+    this.guardarRegistroEntrada(entrada);
+  }
+
+  guardarRegistroEntrada(entrada : any){
     this.svcProdProcesos.Post(entrada).subscribe(data => { 
+    this.crearEtiqueta(data.numero_Rollo, data.peso_Bruto, data.cantidad, data.presentacion);
+    setTimeout(() => {
       this.svcMsjs.mensajeConfirmacion('Confirmación', `Registro de entrada de producción creado con éxito!`);
       this.limpiarCampos();
       this.formSellado.patchValue({ ot : this.ordenConsultada});
-      this.buscarOT();
+      this.buscarOT(); 
+    }, 1000); 
     }, error => this.svcMsjs.mensajeError(`Error`, `No fue posible crear el registro de entrada de producción!`))
   }
 
@@ -182,19 +203,11 @@ export class Produccion_SelladoComponent implements OnInit {
     this.produccion = [];
     this.cargando = false;
     this.esSoloLectura = true;
+    this.clase = ``;
     this.cargarTurnoActual();
   }
   
   aplicarfiltro = ($event, campo : any, valorCampo : string) => this.dtProduccion!.filter(($event.target as HTMLInputElement).value, campo, valorCampo);
-
-  getPuertoUsb(){
-    let device : any;
-    navigator.usb.requestDevice({filters : [{ vendorId : 10473, productId : 643 }]}).then(devices => { 
-      device = devices;
-      console.log(devices)
-      return device.open();
-    });
-  }
 
   habilitarSaldo = () => this.esSoloLectura ? this.esSoloLectura = false : this.esSoloLectura = true;
   
@@ -229,8 +242,6 @@ export class Produccion_SelladoComponent implements OnInit {
             valor = valor.replace(/[^\d.-]/g, '');
             this.formSellado.patchValue({
               cantKg : valor,
-              pesoTeorico : valor,
-              pesoNeto : valor,
             });
           }
         }
@@ -241,4 +252,39 @@ export class Produccion_SelladoComponent implements OnInit {
   }
 
   ab2str = (buf) => String.fromCharCode.apply(null, new Uint8Array(buf));
+
+  crearEtiqueta(rollo : any, cantKg : number, cantUnd : number, medida : any){
+    let proceso : any = this.procesos.find(x => x.Id == this.formSellado.value.proceso);
+    
+    let etiqueta : modelTagProduction = {
+      client: this.ordenesTrabajo[0].cliente,
+      item: this.ordenesTrabajo[0].id_Producto,
+      reference: this.ordenesTrabajo[0].producto,
+      width: this.ordenesTrabajo[0].ancho1_Extrusion,
+      height: this.ordenesTrabajo[0].ancho2_Extrusion,
+      bellows: this.ordenesTrabajo[0].ancho3_Extrusion,
+      und: this.ordenesTrabajo[0].und_Extrusion,
+      cal: this.ordenesTrabajo[0].calibre_Extrusion,
+      orderProduction: this.formSellado.value.ot,
+      material: this.ordenesTrabajo[0].material,
+      quantity: cantUnd,
+      quantity2: cantKg,
+      reel: rollo,
+      presentationItem1: `${medida}(s)`,
+      presentationItem2: 'Kg',
+      productionProcess: proceso.Nombre,
+      showNameBussiness: true,
+    }
+    this.svcCrearPDF.createTagProduction(etiqueta);
+  }
+
+  //Función en desuso
+  getPuertoUsb(){
+    let device : any;
+    navigator.usb.requestDevice({filters : [{ vendorId : 10473, productId : 643 }]}).then(devices => { 
+      device = devices;
+      console.log(devices)
+      return device.open();
+    });
+  }
 }
