280712b31e4ef36a0aa8411209bf0d958b61a374
Háider Cantillo Colón <73559575+Haidercc10@users.noreply.github.com>
2025-07-18 17:35:57 -0500

    Cambios
    Cambios de adición de proceso de perforado.

--- Proyecto/ProyectoPlasticaribe/src/app/Vistas/Produccion_Extrusion/Produccion_Extrusion.component.ts
+++ Proyecto/ProyectoPlasticaribe/src/app/Vistas/Produccion_Extrusion/Produccion_Extrusion.component.ts
@@ -330,7 +330,7 @@ export class Produccion_ExtrusionComponent implements OnInit, OnDestroy {
 
   getProcess() {
     this.processService.srvObtenerLista().subscribe(res => {
-      res.filter(x => ['EXT', 'IMP', 'ROT', 'LAM', 'DBLD', 'CORTE', 'EMP', 'MATPRIMA', 'PERFORADO',].includes(x.proceso_Id)).forEach(process => {
+      res.filter(x => ['EXT', 'IMP', 'ROT', 'LAM', 'DBLD', 'CORTE', 'EMP', 'MATPRIMA', 'PERF',].includes(x.proceso_Id)).forEach(process => {
         this.process.push({
           order: this.sortArrayProcess(process.proceso_Nombre),
           proceso_Id: process.proceso_Id,
